3038f1c93c6da16fa46ef9afb7afecd34f0c30c3
TheRipSon <61383871+TheRipSon@users.noreply.github.com>
2022-08-01 11:38:22 +0200

    clean up

--- main.c
+++ main.c
@@ -9,38 +9,38 @@ static pthread_mutex_t forks[COUNT];
 
 void *philosopher(void *arg) {
     srand(time(NULL));   //init random
-    int i = *(int*)arg;  //convert parameter from void* to int and assign value to i
+    int i = *(int *) arg;  //convert parameter from void* to int and assign value to i
     free(arg); //clear memory allocated in main
-    int z=0;
-    while(z<10) {
+    int z = 0;
+    while (z < 10) {
         z++;
-        if (i == COUNT-1) { // easiest solution one philosopher have different pick order
+        if (i == COUNT - 1) { // easiest solution one philosopher have different pick order
             pthread_mutex_lock(&forks[i]); // lock left fork
-            printf("philosopher %d pick fork %d\n",i,i);
+            printf("philosopher %d pick fork %d\n", i, i);
             pthread_mutex_lock(&forks[(i + 1) % COUNT]);// lock right fork
-            printf("philosopher %d pick fork %d\n",i,(i + 1) % COUNT);
-            printf("philosopher %d eat\n",i,i);
+            printf("philosopher %d pick fork %d\n", i, (i + 1) % COUNT);
+            printf("philosopher %d eat\n", i, i);
             check[i]++; // increment check tab
-            usleep(1+rand()%1000);
+            usleep(1 + rand() % 1000);
             pthread_mutex_unlock(&forks[i]); // release left fork
-            printf("philosopher %d release fork %d\n",i,i);
+            printf("philosopher %d release fork %d\n", i, i);
             pthread_mutex_unlock(&forks[(i + 1) % COUNT]); // release right fork
-            printf("philosopher %d release fork %d\n",i,(i + 1) % COUNT);
+            printf("philosopher %d release fork %d\n", i, (i + 1) % COUNT);
         } else {
             pthread_mutex_lock(&forks[(i + 1) % COUNT]); // lock right fork
-            printf("philosopher %d pick fork %d\n",i,(i + 1) % COUNT);
+            printf("philosopher %d pick fork %d\n", i, (i + 1) % COUNT);
             pthread_mutex_lock(&forks[i]); // lock left fork
-            printf("philosopher %d pick fork %d\n",i,i);
-            printf("philosopher %d eat\n",i,i);
+            printf("philosopher %d pick fork %d\n", i, i);
+            printf("philosopher %d eat\n", i, i);
             check[i]++; // increment check tab
-            usleep(1+rand()%1000);
+            usleep(1 + rand() % 1000);
             pthread_mutex_unlock(&forks[(i + 1) % COUNT]); // release right fork
-            printf("philosopher %d release fork %d\n",i,(i + 1) % COUNT);
+            printf("philosopher %d release fork %d\n", i, (i + 1) % COUNT);
             pthread_mutex_unlock(&forks[i]); // release left fork
-            printf("philosopher %d release fork %d\n",i,i);
+            printf("philosopher %d release fork %d\n", i, i);
         }
-        printf("philosopher %d think\n",i,i);
-        usleep(1+rand()%1000);
+        printf("philosopher %d think\n", i, i);
+        usleep(1 + rand() % 1000);
     }
     return NULL;
 }
@@ -48,8 +48,8 @@ void *philosopher(void *arg) {
 
 int main(int argc, char *argv[]) {
     int i;
-    for(i = 0; i < COUNT; i++) {
-        check[i]=0; //initialize check tab
+    for (i = 0; i < COUNT; i++) {
+        check[i] = 0; //initialize check tab
         pthread_mutex_init(&forks[i], NULL); // init mutex
     }
     pthread_t *tab = (pthread_t *) malloc(sizeof(pthread_t) * COUNT); // reserve memory for threads tab
@@ -65,8 +65,8 @@ int main(int argc, char *argv[]) {
             perror("Failed to join thread");
         }
     }
-    for(i = 0; i < COUNT; i++) {
-        printf("philosopher %d: %d\n",i,check[i]);
+    for (i = 0; i < COUNT; i++) {
+        printf("philosopher %d: %d\n", i, check[i]);
         pthread_mutex_destroy(&forks[i]); // free memory
     }
     return 0;
